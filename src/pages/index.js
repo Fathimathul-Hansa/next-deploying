51b2b797bb9571244b7c70519c9a55d8e6560d5a
Hansa <hansa@kiebot.com>
2026-09-23 09:48:43 +0400

    modified batch-36

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,12 +18,12 @@ export default function Home() {
        <div>this is sample deployment nextjs
         <h1>demo-deploy</h1>
         <img src='https://images.pexels.com/photos/35802444/pexels-photo-35802444.jpeg' width={300}/>
-        <img src='https://images.pexels.com/photos/37186779/pexels-photo-37186779.jpeg' width={500}/>
+        {/* <img src='https://images.pexels.com/photos/37186779/pexels-photo-37186779.jpeg' width={500}/> */}
         <h2>demo tooooo</h2>
         <h1 >hii batch</h1>
         <h6>batch.....going on</h6>
-        <h6>batch.....going on...;..test-30</h6>
-        <h5>batch-30</h5>
+        <h6>batch.....going on...;..test-36</h6>
+        {/* <h5>batch-30</h5> */}
 
        </div>
       </main>
